26f5cfc87f8561fd85dd33c4e0099eaf6777bb19
tupy77 <tupy77@hotmail.com>
2023-05-17 00:20:01 +0200

    configuracion de spider y agregar autor de quotes

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -2,6 +2,7 @@ import scrapy
 
 # Titulo = //h1/a/text()
 # Cita = //span[@class="text" and @itemprop="text"]/text()
+# Autor = //small[@class="author" and @itemprop="author"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
 # Next page button = //li[@class="next"]/a/@href
 
@@ -16,12 +17,12 @@ class QuotesSpyder(scrapy.Spider):
     custom_settings = {
         'FEED_URI': 'quotes.json',
         'FEED_FORMAT': 'json',
-        #'CONCURRENT_REQUESTS': 24,
-        #'MEMUSAGE_LIMIT_MB': 2048,
-        #'MEMUSAGE_NOTIFY_MAIL': ['email'],
-        #'ROBOTSTXT_OBEY': True,
-        #'USER_AGENT': 'QuotesSpyder (http://quotes.toscrape.com)',
-        #'FEED_EXPORT_ENCODING': 'utf-8',
+        'CONCURRENT_REQUESTS': 30,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['tupy77@hotmail.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'QuotesSpyder test',
+        'FEED_EXPORT_ENCODING': 'utf-8',
         #'HTTPCACHE_ENABLED': True,
         #'HTTPCACHE_EXPIRATION_SECS': 86400,
         #'HTTPCACHE_DIR': 'httpcache',
@@ -38,14 +39,17 @@ class QuotesSpyder(scrapy.Spider):
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
             quotes = kwargs['quotes']
+            author = kwargs['author']
         quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+        author.extend(response.xpath('//small[@class="author" and @itemprop="author"]/text()').getall())
 
         next_page_button = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page_button:
-            yield response.follow(next_page_button, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'author': author})
         else:
             yield {
-                'quotes': quotes
+                'quotes': quotes,
+                'author': author
             }
 
     def parse(self, response):
@@ -54,19 +58,17 @@ class QuotesSpyder(scrapy.Spider):
             '//h1/a/text()').get()        
         quotes = response.xpath(
             '//span[@class="text" and @itemprop="text"]/text()').getall()
+        author = response.xpath(
+            '//small[@class="author" and @itemprop="author"]/text()').getall()
         top_tags = response.xpath(
             '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
         
         
-        top = getattr(self, 'top', None) # top es un atributo de la clase QuotesSpyder. Si existe, se le asigna a top, si no, se le asigna None
+        top = getattr(self, 'top', None) # getattr() es una funcion de python que permite obtener un atributo de un objeto
 
         if top:
             top = int(top)
             top_tags = top_tags[:top]
-
-        # Luego, en la consola, se ejecuta:
-        # scrapy crawl quotes -a top=3
-        # para que solo se muestren las 3 primeras etiquetas
 
         yield {
             'title': title,
@@ -75,5 +77,5 @@ class QuotesSpyder(scrapy.Spider):
 
         next_page_button = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page_button:
-            yield response.follow(next_page_button, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'author': author})
 
